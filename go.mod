96b70ccb1ca37badf6dbd40352db4dce2a573951
Andrey Afoninskiy <andrey.afoninskiy@mambu.com>
2021-04-25 20:55:48 +0200

    WIP: temporary commit [skip CI]

--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.15
 require (
 	github.com/VictoriaMetrics/metrics v1.17.2
 	github.com/afoninsky-go/logger v0.0.2
+	github.com/afoninsky/utilities v0.0.0-20201226091459-49dbbda397eb
+	github.com/afoninsky/verdite v0.0.0-20210111133424-96260c5df539
 	github.com/getkin/kin-openapi v0.54.0
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/golang-lru v0.5.4
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/stretchr/testify v1.5.1
 )
--- service/service.go
+++ service/service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/afoninsky-go/hhistogram/metric"
 	"github.com/afoninsky-go/hhistogram/openapi"
@@ -11,6 +14,7 @@ import (
 )
 
 const histogramName = "test"
+const endpoint = "http://localhost:8081"
 
 type Service struct {
 	log *logger.Logger
@@ -21,7 +25,6 @@ func NewHistogramService() *Service {
 	s := &Service{}
 	s.log = logger.NewSTDLogger()
 	s.api = openapi.NewURLParser().WithLogger(s.log)
-
 	return s
 }
 
@@ -30,16 +33,81 @@ func (s *Service) BulkHandler(w http.ResponseWriter, r *http.Request) {
 	// create bulk processor to convert stream of http events into set of histograms
 	config := processor.NewConfig().WithName(histogramName)
 	bulk := processor.NewHistogramProcessor(config).WithInterceptor(s).WithLogger(s.log)
-	// stream JSONP events
+
+	// process incoming metrics
 	if err := bulk.ReadFromStream(r.Body); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// display result
-	bulk.Process(w)
-	// fmt.Fprintf(w, "OK")
+	// get remote endpoint with passed query args
+	u, _ := url.Parse(endpoint)
+	u.RawQuery = r.URL.RawQuery
+
+	// proxy processor response to endpoint
+	buf := new(bytes.Buffer)
+	bulk.Process(buf)
+	proxyReq, _ := http.NewRequest(http.MethodPost, u.String(), buf)
+	defer proxyReq.Body.Close()
+	httpClient := &http.Client{
+		Timeout: time.Second * 60,
+	}
+	proxyRes, err := httpClient.Do(proxyReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer proxyRes.Body.Close()
+
+	// send back response status code
+	w.WriteHeader(proxyRes.StatusCode)
+	bodyBytes, _ := ioutil.ReadAll(proxyRes.Body)
+	w.Write(bodyBytes)
 }
+
+// func copyHeaders(dst, src http.Header) {
+// 	for k, vv := range src {
+// 		for _, v := range vv {
+// 			dst.Add(k, v)
+// 		}
+// 	}
+// }
+
+// func httpForwarder(w http.ResponseWriter, r *http.Request) {
+// 	res, err := http.DefaultTransport.RoundTrip(r)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+// 		return
+// 	}
+// 	defer res.Body.Close()
+// 	copyHeaders(w.Header(), res.Header)
+// 	w.WriteHeader(res.StatusCode)
+// 	if _, err := io.Copy(w, res.Body); err != nil {
+// 		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+// 		return
+// 	}
+// }
+
+// func (s *Service) streamResponse() {
+// 	httpClient := &http.Client{}
+// 	req, _ := http.NewRequest(httpMethod, fmt.Sprintf("%s/?region=%s", cfg.Endpoint, record.AWSRegion), obj.Body)
+// 	httpUserAgent := fmt.Sprintf("kourier-lambda/%s", cfg.Version)
+// 	referer := fmt.Sprintf("s3://%s/%s", record.S3.Bucket.Name, record.S3.Object.Key)
+// 	req.Header.Set("Content-Encoding", httpContentEncoding)
+// 	req.Header.Set("User-Agent", httpUserAgent)
+// 	req.Header.Set("Referer", referer)
+// 	res, err := httpClient.Do(req)
+// 	if err != nil {
+// 		return errors.Wrap(err, "http")
+
+// 	}
+// 	defer res.Body.Close()
+// 	if res.StatusCode != http.StatusOK {
+// 		return errors.Wrap(fmt.Errorf("invalid response status - %d", res.StatusCode), "http")
+// 	}
+// 	return nil
+
+// }
 
 // convert http event to obfuscated metric
 func (s *Service) OnMetric(m *metric.Metric) error {
