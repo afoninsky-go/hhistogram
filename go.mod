73340ab3db4b26313767c8303e4cdb7e293bb3ea
Andrey Afoninskiy <andrey.afoninskiy@mambu.com>
2021-04-26 16:28:50 +0200

    WIP: temporary commit [skip CI]

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.15
 require (
 	github.com/VictoriaMetrics/metrics v1.17.2
 	github.com/afoninsky-go/logger v0.0.2
-	github.com/afoninsky/utilities v0.0.0-20201226091459-49dbbda397eb
-	github.com/afoninsky/verdite v0.0.0-20210111133424-96260c5df539
 	github.com/getkin/kin-openapi v0.54.0
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/golang-lru v0.5.4
-	github.com/julienschmidt/httprouter v1.3.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.5.1
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/afoninsky-go/hhistogram/service"
@@ -10,21 +11,25 @@ import (
 )
 
 const httpTimeout = 15 * time.Second
-const httpListenAddr = "127.0.0.1:8080"
 
 func main() {
+	httpListenAddr := fromEnv("HTTP_LISTEN", "127.0.0.1:8080")
+	httpReceiver := fromEnv("HTTP_RECEIVER", "http://localhost:8081")
+	hName := fromEnv("NAME", "test_histogram")
+	specFolder := fromEnv("SPEC_PATH", "./test/json")
+
 	log := logger.NewSTDLogger()
 	router := mux.NewRouter()
 	histogram, err := service.NewHistogramService(service.Config{
-		HistogramName:  "test",
-		OutputEndpoint: "http://localhost:8081",
-		SpecFolder:     "./test/json",
+		HistogramName:  hName,
+		OutputEndpoint: httpReceiver,
+		SpecFolder:     specFolder,
 	})
 	log.FatalIfError(err)
 
 	router.Use(log.CreateMiddleware())
 	router.Path("/bulk").Methods(http.MethodPost).HandlerFunc(histogram.BulkHandler)
-	router.Path("/health").Methods(http.MethodGet).HandlerFunc(histogram.HealthHandler)
+	router.Path("/health").Methods(http.MethodGet, http.MethodHead).HandlerFunc(histogram.HealthHandler)
 	log.CreateMiddleware()
 
 	srv := &http.Server{
@@ -33,6 +38,15 @@ func main() {
 		WriteTimeout: httpTimeout,
 		ReadTimeout:  httpTimeout,
 	}
-	log.Infof("Running server on %s ...", httpListenAddr)
+	log.Infof("Server on %s ...", httpListenAddr)
+	log.Infof(`Generate histogram "%s" to %s ...`, hName, httpReceiver)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func fromEnv(envName, defValue string) string {
+	envValue, ok := os.LookupEnv(envName)
+	if ok {
+		return envValue
+	}
+	return defValue
 }
